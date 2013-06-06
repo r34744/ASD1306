4020db13f0976e0c6524e2f61057f28adefb7731
Greg Koenig <gkoenig@fullsail.edu>
2013-06-06 08:52:02 -0400

    js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 // Gregory Koenig
-// ASD 1305
+// ASD 1306
 
 
+$('#main').on('pageinit', function(){
+	//code needed for home page goes here
+});
+
+$('#AddNewRace').on('pageinit', function(){
+	//code needed for home page goes here
+});
+
+$('#PastRaces').on('pageinit', function(){
+	//code needed for home page goes here
+});
+
+$('#about').on('pageinit', function(){
+	//code needed for home page goes here
+});	
